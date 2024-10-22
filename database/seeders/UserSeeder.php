026883cb7126daa9969c21a492f6e356dd5eb0f6
LeeMangold <lmangold@outlook.com>
2024-10-21 20:00:56 -0400

    Fixed Seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -12,35 +12,13 @@ class UserSeeder extends Seeder
     {
         // Manually create the first user
         User::create([
-            'name' => 'Lee Mangold',
-            'email' => 'lmangold@outlook.com',
-            'email_verified_at' => now(),
-            'password' => bcrypt('lee5882'), // Choose a default password
-        ]);
-
-        User::create([
-            'name' => 'Ashley Archibald',
-            'email' => 'asharris89@outlook.com',
+            'name' => 'Admin User',
+            'email' => 'admin@example.com',
             'email_verified_at' => now(),
             'password' => bcrypt('password'), // Choose a default password
         ]);
-
-        User::create([
-            'name' => 'Jake Harris',
-            'email' => 'jrharris131@gmail.com',
-            'email_verified_at' => now(),
-            'password' => bcrypt('EatADickJake69!'), // Choose a default password
-        ]);
-
-        User::create([
-            'name' => 'Jace Powell',
-            'email' => 'jace@example.com',
-            'email_verified_at' => now(),
-            'password' => bcrypt('password'), // Choose a default password
-        ]);
-
 
         // Use the factory to create 5 more users
-        User::factory()->count(5)->create();
+        //User::factory()->count(5)->create();
     }
 }
